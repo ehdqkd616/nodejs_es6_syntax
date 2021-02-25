bc308ced8cd0bf88ff6d2fbbd0eb814cb266af6c
Rudy <my4my4@gmail.com>
2021-02-25 16:34:38 +0900

    ES6

--- js/class_SetGet.js
+++ js/class_SetGet.js
@@ -4,19 +4,17 @@ class Person {
         this.name = name;
     }
 
-    get id(){
+    getId(){
         return this.id;
     }
-
-    get name(){
+    getName(){
         return this.name;
     }
 
-    set id(newId){
+    setId(newId){
         this.id = newId;
     }
-
-    set name(newName){
+    setName(newName){
         this.name = newName;
     }
 
@@ -30,19 +28,22 @@ class Student extends Person {
         super(id, name);
         this.age = age;
     }
-    get age(){
+    
+    getAge(){
         return this.age;
     }
-
-    set age(newAge){
+    
+    setAge(newAge){
         this.age = newAge;
     }
+    
     toString() {
         return super.toString() + ' and age = ' + this.age;
     }
 }
 
 let person = new Person('person', '사람');
+console.log('--------------Person--------------');
 console.log(person.toString());
 
 person.setId('set_person');
@@ -51,6 +52,7 @@ person.setName('set_name');
 console.log(person.toString());
 
 let student = new Student('student', '학생', 25);
+console.log('--------------student--------------');
 console.log(student.toString());
 
 student.setId('set_person');
@@ -58,3 +60,13 @@ student.setName('set_name');
 student.setAge(30);
 
 console.log(student.toString());
+
+console.log('--------------student.get--------------');
+console.log('student.getId() : '+student.getId());
+console.log('student.getName() : '+student.getName());
+console.log('student.getAge() : '+student.getAge());
+
+console.log('--------------student.field--------------');
+console.log('student.id : '+student.id);
+console.log('student.name() : '+student.name);
+console.log('student.age() : '+student.age);
--- js/function_form3.js
+++ js/function_form3.js
@@ -2,7 +2,7 @@ function NumberEx2() {
     this.num = 0;
     
     setInterval(() => {
-        this.num++; // this is from NumberEx
+        this.num++; // this is from NumberEx2
         console.log(this.num);
     }, 500)
 }
